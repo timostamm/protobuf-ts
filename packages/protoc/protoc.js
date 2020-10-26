95f1805f11b9dff32055033ce4d3b2d06e9e1388
Timo Stamm <ts@timostamm.de>
2020-10-26 12:34:20 +0100

    Typo fix, formatting, doc.

--- packages/protoc/protoc.js
+++ packages/protoc/protoc.js
@@ -20,47 +20,44 @@ const os = require('os');
 const {findProtocVersionConfig, listInstalled, findProtocPlugins, findProtobufTs, unzip, makeReleaseName, httpGetRedirect, httpDownload, mkDirRecursive, standardInstallDirectory} = require('./util');
 
 
-// search for a configured specific protoc version
-let version = findProtocVersionConfig(process.cwd());
+ensureInstalled(findProtocVersionConfig(process.cwd()))
+    .then(release => {
+        let command = release.protocPath;
+        let args = [
+            // pass all arguments to the process
+            ...process.argv.slice(2),
+            // add the "include" directory of the installed protoc to the proto path
+            // do this last, otherwise it can shadow a user input
+            "--proto_path", release.includePath,
+        ];
 
+        // search for @protobuf-ts/plugin in node_modules and add --proto_path argument
+        let protobufTs = findProtobufTs(process.cwd());
+        if (protobufTs) {
+            args.push("--proto_path", protobufTs);
+        }
 
-ensureInstalled(version).then(release => {
-    let command = release.protocPath;
-    let args = [
-        // pass all arguments to the process
-        ...process.argv.slice(2),
-        // add the "include" directory of the installed protoc to the proto path
-        // do this last, otherwise it can shadow a user input
-        "--proto_path", release.includePath,
-    ];
+        // search for any protoc-gen-xxx plugins in .bin and add --plugin arguments for them
+        for (let plugin of findProtocPlugins(process.cwd())) {
+            args.unshift("--plugin", plugin);
+        }
 
-    // search for @protobuf-ts/plugin in node_modules and add --proto_path argument
-    let protobufTs = findProtobufTs(process.cwd());
-    if (protobufTs) {
-        args.push("--proto_path", protobufTs);
-    }
+        let child = spawnSync(command, args, {
+            // protoc accepts stdin for some commands, pipe all IO
+            stdio: [process.stdin, process.stdout, process.stderr],
+            shell: false
+        });
 
-    // search for any protoc-gen-xxx plugins in .bin and add --plugin arguments for them
-    for (let plugin of findProtocPlugins(process.cwd())) {
-        args.unshift("--plugin", plugin);
-    }
+        if (child.error) {
+            throw new Error("@protobuf-ts/protoc was unable to spawn protoc. " + child.error);
+        }
+        process.exit(child.status);
 
-    let child = spawnSync(command, args, {
-        // protoc accepts stdin for some commands, pipe all IO
-        stdio: [process.stdin, process.stdout, process.stderr],
-        shell: false
+    })
+    .catch(err => {
+        console.error((err instanceof Error) ? err.message : err);
+        process.exit(1);
     });
-
-    if (child.error) {
-        throw new Error("@protobuf-ts/protoc was unable to spawn protoc. " + child.error);
-    }
-    process.exit(child.status);
-
-}).catch(err => {
-    console.error((err instanceof Error) ? err.message : err);
-    process.exit(1);
-});
-
 
 
 async function ensureInstalled(version) {
@@ -70,7 +67,7 @@ async function ensureInstalled(version) {
         try {
             latestLocation = await httpGetRedirect("https://github.com/protocolbuffers/protobuf/releases/latest");
         } catch (e) {
-            throw new Error(`@protobuf-ts/protoc failed to retreive lastest protoc version number: ${e}`);
+            throw new Error(`@protobuf-ts/protoc failed to retrieve latest protoc version number: ${e}`);
         }
         version = latestLocation.split("/v").pop();
     }
@@ -82,6 +79,7 @@ async function ensureInstalled(version) {
         version: version
     });
 
+    // if this release is already installed, we are done here
     let alreadyInstalled = listInstalled().find(i => i.name === releaseName);
     if (alreadyInstalled) {
         return alreadyInstalled;
@@ -94,22 +92,29 @@ async function ensureInstalled(version) {
     } catch (e) {
         throw new Error(`@protobuf-ts/protoc failed to download protoc v${version}. \nDid you misspell the version number? The version number must look like "3.0.12", without a leading "v".\n${e}`);
     }
-    let archivePath = path.join(standardInstallDirectory, releaseName);
-    unzip(archive, (data, header) => {
-        let filename = path.join(archivePath, header.filename);
-        mkDirRecursive(path.dirname(filename));
-        fs.writeFileSync(filename, data, {
-            mode: header.filename.includes("bin/") ? 0o755 : 0o666
-        });
-    });
 
+    // unzip the archive
+    let archivePath = path.join(standardInstallDirectory, releaseName);
+    try {
+        unzip(archive, (data, header) => {
+            let filename = path.join(archivePath, header.filename);
+            mkDirRecursive(path.dirname(filename));
+            fs.writeFileSync(filename, data, {
+                mode: header.filename.includes("bin/") ? 0o755 : 0o666
+            });
+        });
+    } catch (e) {
+        throw new Error(`@protobuf-ts/protoc failed unzip the downloaded protoc release v${version}: ${e}`);
+    }
+
+    // sanity check
     let installed = listInstalled().find(i => i.name === releaseName);
     if (!installed) {
         throw new Error(`@protobuf-ts/protoc failed to install protoc v${version}.`);
     }
 
+    // finished
     console.info(`@protobuf-ts/protoc installed protoc v${installed.version}.`);
-
     return installed;
 }
 
