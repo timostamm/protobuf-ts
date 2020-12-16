3dc7e24805740b480f224fe923b99f54100d32a5
Timo Stamm <ts@timostamm.de>
2020-12-16 18:33:43 +0100

    #45 fix CI

--- packages/grpc-transport/spec/grpc-transport.spec.ts
+++ packages/grpc-transport/spec/grpc-transport.spec.ts
@@ -1,10 +1,12 @@
 import {GrpcTransport} from "../src";
+import {ChannelCredentials} from "@grpc/grpc-js";
 
 describe('GrpcTransport', () => {
 
     it('can be created', function () {
         let transport = new GrpcTransport({
-            baseUrl: "fake",
+            host: "fake",
+            channelCredentials: ChannelCredentials.createInsecure()
         });
         expect(transport).toBeDefined();
     });
