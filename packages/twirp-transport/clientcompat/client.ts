f65a571c6c583a08a2f38ccd40551953ba6ea7d9
Timo Stamm <ts@timostamm.de>
2021-05-07 14:52:42 +0200

    twirp v8.0.0 clientcompat workaround
    The latest twirp release breaks our clientcompat checks.
    Verified code and confirmed that there is an integration issue triggered by v8.0.0.
    
    Changed tests to run with v7 for now.

--- packages/twirp-transport/clientcompat/client.ts
+++ packages/twirp-transport/clientcompat/client.ts
@@ -1,17 +1,17 @@
 import {ReadStream} from "tty";
 import {ClientCompatMessage, ClientCompatMessage_CompatServiceMethod, Empty, Req, Resp} from "./clientcompat";
-import {TwirpFetchTransport, TwirpOptions} from "../";
-import {FinishedUnaryCall, RpcError} from "@protobuf-ts/runtime-rpc";
+import {TwirpFetchTransport} from "../";
+import {RpcError} from "@protobuf-ts/runtime-rpc";
 import {default as fetch, Headers} from "node-fetch";
-import {CompatServiceClient} from "./clientcompat.client";
+import {CompatServiceClient, ICompatServiceClient} from "./clientcompat.client";
+import * as fs from "fs";
+import * as path from "path";
+
 
 // fetch polyfill via https://github.com/node-fetch/node-fetch
 globalThis.fetch = fetch;
 globalThis.Headers = Headers;
 
-let options: TwirpOptions = {
-    baseUrl: "coming soon",
-};
 
 /**
  * Testing compatibility of our Twirp transport with the protocol by using
@@ -23,37 +23,91 @@ export async function client() {
 
     // "The client binary must accept, over stdin, a protobuf-encoded ClientCompatMessage."
     let message = ClientCompatMessage.fromBinary(await readBytes(process.stdin));
+    log('client start with message: ' + ClientCompatMessage.toJsonString(message, {emitDefaultValues: true}));
 
-    // "This message contains a service_address, which is the address of a reference-
-    // implementation CompatService to talk to."
-    options.baseUrl = `${message.serviceAddress}/twirp`;
-    let client = new CompatServiceClient(new TwirpFetchTransport(options));
+    let client = new CompatServiceClient(new TwirpFetchTransport({
+        // "This message contains a service_address, which is the address of a reference-
+        // implementation CompatService to talk to."
+        baseUrl: `${message.serviceAddress}/twirp`
+    }));
 
-    // "an embedded, proto-encoded request which the client should send."
-    let request = Req.fromBinary(message.request);
+    // method: "a method, which specifies which RPC of the CompatService should be called"
+    switch (message.method) {
+        case ClientCompatMessage_CompatServiceMethod.METHOD:
+            await doMethod(client, message.request);
+            break;
+        case ClientCompatMessage_CompatServiceMethod.NOOP:
+            await doNoop(client, message.request);
+            break;
+        default:
+            throw new Error('client does not support method');
+    }
+
+    log('client exit');
+}
+
+
+async function doNoop(client: ICompatServiceClient, req: Uint8Array) {
+    // request: "an embedded, proto-encoded request which the client should send."
+    log('request raw: ' + byteArrToStr(req));
+    let request = Empty.fromBinary(req);
+    log('request (Empty) as json: ' + Empty.toJsonString(request, {emitDefaultValues: true}));
+    log('request (Empty) as protobuf: ' + byteArrToStr(Empty.toBinary(request)));
 
     try {
-        // "a method, which specifies which RPC of the CompatService should be called"
-        let call: FinishedUnaryCall<Req, Resp> | FinishedUnaryCall<Empty, Empty> =
-            message.method === ClientCompatMessage_CompatServiceMethod.METHOD
-                ? (await client.method(request))
-                : (await client.noopMethod(request));
+
+        log('calling CompatService.NoopMethod(Empty)');
+        let {response} = await client.noopMethod(request);
+        log('response (Empty) as json: ' + Empty.toJsonString(response, {emitDefaultValues: true}));
+        log('response (Empty) as protobuf: ' + byteArrToStr(Empty.toBinary(response)));
 
         // "If the server doesn't send an error, the client should encode the response
         // message it received as protobuf and write it to stdout."
-        let bytes = call.method.O.toBinary(call.response);
-        process.stdout.write(bytes);
+        process.stdout.write(Empty.toBinary(response));
+        log('wrote response to stdout as protobuf')
 
     } catch (e) {
         // "If the server sends an error, then the client should parse the error and
         // write the error code string ("internal", "unauthenticated", etc) to stderr."
         if (e instanceof RpcError) {
             process.stderr.write(e.code);
+            log('request failed, wrote code to stdout: ' + e.code);
         } else {
-            process.stderr.write(`caught unexpected error: ${e}`);
+            throw e;
         }
     }
+}
 
+async function doMethod(client: ICompatServiceClient, req: Uint8Array) {
+    // request: "an embedded, proto-encoded request which the client should send."
+    log('request raw: ' + byteArrToStr(req));
+
+    let request = Req.fromBinary(req);
+    log('request (Req) as json: ' + Req.toJsonString(request, {emitDefaultValues: true}));
+    log('request (Req) as protobuf: ' + byteArrToStr(Req.toBinary(request)));
+
+    try {
+
+        log('calling CompatService.Method(Req)');
+        let {response} = await client.method(request);
+        log('response (Resp) as json: ' + Resp.toJsonString(response, {emitDefaultValues: true}));
+        log('response (Resp) as protobuf: ' + byteArrToStr(Resp.toBinary(response)));
+
+        // "If the server doesn't send an error, the client should encode the response
+        // message it received as protobuf and write it to stdout."
+        process.stdout.write(Resp.toBinary(response));
+        log('wrote response to stdout as protobuf.')
+
+    } catch (e) {
+        // "If the server sends an error, then the client should parse the error and
+        // write the error code string ("internal", "unauthenticated", etc) to stderr."
+        if (e instanceof RpcError) {
+            process.stderr.write(e.code);
+            log('request failed, wrote code to stdout: ' + e.code);
+        } else {
+            throw e;
+        }
+    }
 }
 
 
@@ -65,4 +119,21 @@ function readBytes(stream: ReadStream): Promise<Uint8Array> {
             resolve(Buffer.concat(chunks));
         });
     });
+}
+
+function byteArrToStr(bytes: Uint8Array): string {
+    let r: string[] = [];
+    bytes.forEach(b => {
+        let s = b.toString(16).toUpperCase();
+        s = '0x' + "0".repeat(2 - s.length) + s;
+        r.push(s);
+    });
+    return r.join(', ') + ` (${r.length} bytes)`;
+}
+
+function log(message: string): void {
+    fs.appendFileSync(
+        path.join(__dirname, '../clientcompat.log'),
+        message + '\n'
+    );
 }
