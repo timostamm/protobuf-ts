026008d7e2df12d5090250c889b015e9c37e10d1
Timo Stamm <ts@timostamm.de>
2025-03-13 01:22:59 +0100

    Check for diff in CI (#685)

---
+++ scripts/gh-diffcheck.js
@@ -0,0 +1,25 @@
+const { execSync } = require("node:child_process");
+
+if (gitUncommitted()) {
+    process.stdout.write(
+        "Uncommitted changes found:\n",
+    );
+    execSync("git status --porcelain", {
+        stdio: "inherit",
+    });
+    execSync("git --no-pager diff", {
+        stdio: "inherit",
+    });
+    process.exit(1);
+}
+
+/**
+ * @returns {boolean}
+ */
+function gitUncommitted() {
+    const out = execSync("git status --porcelain", {
+        encoding: "utf-8",
+    });
+    return out.trim().length > 0;
+}
+
