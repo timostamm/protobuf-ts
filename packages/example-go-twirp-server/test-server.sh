c5b76ce98d52f67233227944766e1925fc9520a4
Timo Stamm <ts@timostamm.de>
2025-03-24 22:11:13 +0100

    Replace example-go-twirp-server
    Adds a minimal node twirp service in example-node-twirp-transport-client instead.

--- packages/example-go-twirp-server/test-server.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-curl \
-  --request POST \
-  --header "Content-Type: application/json" \
-  --data '{"inches":23}' \
-  http://localhost:8080/twirp/spec.haberdasher.Haberdasher/MakeHat
