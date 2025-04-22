d76425146f2ca1d2ab1b6adb1cf7c2376ac6936f
Timo Stamm <ts@timostamm.de>
2025-04-23 00:08:07 +0200

    Force re-generate
    Probably a tainted local cache..

--- packages/test-force_optimize_speed-long_type_string/gen/google/protobuf/wrappers.ts
+++ packages/test-force_optimize_speed-long_type_string/gen/google/protobuf/wrappers.ts
@@ -32,10 +32,17 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
-// Wrappers for primitive (non-message) types. These types are useful
-// for embedding primitives in the `google.protobuf.Any` type and for places
-// where we need to distinguish between the absence of a primitive
-// typed field and its default value.
+// Wrappers for primitive (non-message) types. These types were needed
+// for legacy reasons and are not recommended for use in new APIs.
+//
+// Historically these wrappers were useful to have presence on proto3 primitive
+// fields, but proto3 syntax has been updated to support the `optional` keyword.
+// Using that keyword is now the strongly preferred way to add presence to
+// proto3 primitive fields.
+//
+// A secondary usecase was to embed primitives in the `google.protobuf.Any`
+// type: it is now recommended that you embed your value in your own wrapper
+// message which can be specifically documented.
 //
 // These wrappers have no meaningful use within repeated fields as they lack
 // the ability to detect presence on individual elements.
@@ -61,6 +68,9 @@ import { MessageType } from "@protobuf-ts/runtime";
  *
  * The JSON representation for `DoubleValue` is JSON number.
  *
+ * Not recommended for use in new APIs, but still useful for legacy APIs and
+ * has no plan to be removed.
+ *
  * @generated from protobuf message google.protobuf.DoubleValue
  */
 export interface DoubleValue {
@@ -75,6 +85,9 @@ export interface DoubleValue {
  * Wrapper message for `float`.
  *
  * The JSON representation for `FloatValue` is JSON number.
+ *
+ * Not recommended for use in new APIs, but still useful for legacy APIs and
+ * has no plan to be removed.
  *
  * @generated from protobuf message google.protobuf.FloatValue
  */
@@ -91,6 +104,9 @@ export interface FloatValue {
  *
  * The JSON representation for `Int64Value` is JSON string.
  *
+ * Not recommended for use in new APIs, but still useful for legacy APIs and
+ * has no plan to be removed.
+ *
  * @generated from protobuf message google.protobuf.Int64Value
  */
 export interface Int64Value {
@@ -105,6 +121,9 @@ export interface Int64Value {
  * Wrapper message for `uint64`.
  *
  * The JSON representation for `UInt64Value` is JSON string.
+ *
+ * Not recommended for use in new APIs, but still useful for legacy APIs and
+ * has no plan to be removed.
  *
  * @generated from protobuf message google.protobuf.UInt64Value
  */
@@ -121,6 +140,9 @@ export interface UInt64Value {
  *
  * The JSON representation for `Int32Value` is JSON number.
  *
+ * Not recommended for use in new APIs, but still useful for legacy APIs and
+ * has no plan to be removed.
+ *
  * @generated from protobuf message google.protobuf.Int32Value
  */
 export interface Int32Value {
@@ -135,6 +157,9 @@ export interface Int32Value {
  * Wrapper message for `uint32`.
  *
  * The JSON representation for `UInt32Value` is JSON number.
+ *
+ * Not recommended for use in new APIs, but still useful for legacy APIs and
+ * has no plan to be removed.
  *
  * @generated from protobuf message google.protobuf.UInt32Value
  */
@@ -151,6 +176,9 @@ export interface UInt32Value {
  *
  * The JSON representation for `BoolValue` is JSON `true` and `false`.
  *
+ * Not recommended for use in new APIs, but still useful for legacy APIs and
+ * has no plan to be removed.
+ *
  * @generated from protobuf message google.protobuf.BoolValue
  */
 export interface BoolValue {
@@ -166,6 +194,9 @@ export interface BoolValue {
  *
  * The JSON representation for `StringValue` is JSON string.
  *
+ * Not recommended for use in new APIs, but still useful for legacy APIs and
+ * has no plan to be removed.
+ *
  * @generated from protobuf message google.protobuf.StringValue
  */
 export interface StringValue {
@@ -180,6 +211,9 @@ export interface StringValue {
  * Wrapper message for `bytes`.
  *
  * The JSON representation for `BytesValue` is JSON string.
+ *
+ * Not recommended for use in new APIs, but still useful for legacy APIs and
+ * has no plan to be removed.
  *
  * @generated from protobuf message google.protobuf.BytesValue
  */
