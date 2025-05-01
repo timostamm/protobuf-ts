c820aee304d9050f57de7266ac1f025b42b8b84c
Timo Stamm <ts@timostamm.de>
2025-05-01 20:44:19 +0200

    Fix publish.bash

--- scripts/publish.bash
+++ scripts/publish.bash
@@ -3,12 +3,12 @@
 npm run all
 npx turbo run bootstrap
 
-cd packages/runtime; npm publish
-cd packages/runtime-rpc; npm publish
-cd packages/plugin; npm publish
-cd packages/plugin-framework; npm publish
-cd packages/protoc; npm publish
-cd packages/twirp-transport; npm publish
-cd packages/grpcweb-transport; npm publish
-cd packages/grpc-transport; npm publish
-cd packages/grpc-backend; npm publish
+cd packages/runtime; npm publish; cd ../../;
+cd packages/runtime-rpc; npm publish; cd ../../;
+cd packages/plugin; npm publish; cd ../../;
+cd packages/plugin-framework; npm publish; cd ../../;
+cd packages/protoc; npm publish; cd ../../;
+cd packages/twirp-transport; npm publish; cd ../../;
+cd packages/grpcweb-transport; npm publish; cd ../../;
+cd packages/grpc-transport; npm publish; cd ../../;
+cd packages/grpc-backend; npm publish; cd ../../;
