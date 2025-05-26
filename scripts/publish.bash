c8dc9aecf7916e682f377ed22cd30557c2991529
Timo Stamm <ts@timostamm.de>
2025-05-26 13:30:01 +0200

    Remove deprecated @protobuf ts/plugin framework (#721)

--- scripts/publish.bash
+++ scripts/publish.bash
@@ -2,13 +2,11 @@
 
 set -e
 
-npx turbo run bootstrap
 npm run all
 
 cd packages/runtime; npm publish; cd ../../;
 cd packages/runtime-rpc; npm publish; cd ../../;
 cd packages/plugin; npm publish; cd ../../;
-cd packages/plugin-framework; npm publish; cd ../../;
 cd packages/protoc; npm publish; cd ../../;
 cd packages/twirp-transport; npm publish; cd ../../;
 cd packages/grpcweb-transport; npm publish; cd ../../;
