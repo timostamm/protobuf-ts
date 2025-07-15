42f31e1a90fb0583bf504b0811bb53d047e8d5d2
Timo Stamm <ts@timostamm.de>
2025-07-15 22:20:10 +0200

    Add tests for generated code

---
+++ packages/test-long_type_string/spec/message-type.spec.ts
@@ -0,0 +1,23 @@
+import {Duration} from "../gen/google/protobuf/duration";
+import {Timestamp} from "../gen/google/protobuf/timestamp";
+
+// Copied from test-default/message-type.spec.ts. Do not edit.
+
+describe('MessageType', function () {
+    it('is()', () => {
+        const dur = Duration.create();
+        const tim = Timestamp.create();
+        expect( Duration.is(dur)).toBe(true);
+        expect( Timestamp.is(tim)).toBe(true);
+        expect( Duration.is(tim)).toBe(false);
+        expect( Timestamp.is(dur)).toBe(false);
+    });
+    it('isAssignable()', () => {
+        const dur = Duration.create();
+        const tim = Timestamp.create();
+        expect( Duration.isAssignable(dur)).toBe(true);
+        expect( Timestamp.isAssignable(tim)).toBe(true);
+        expect( Duration.isAssignable(tim)).toBe(true);
+        expect( Timestamp.isAssignable(dur)).toBe(true);
+    });
+});
